fb0fd8661910dc695242dd8b3bfe89fd7c7e79d0
knight <wk_knight@163.com>
2022-01-05 20:04:16 +0800

    linknode test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+# from django.test import TestCase
 
 # Create your tests here.
+class LinkNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+head = LinkNode("h")
+second = LinkNode("e")
+third = LinkNode("l")
+fourth = LinkNode("l")
+fifth = LinkNode("o")
+tail = LinkNode(None)
+head.next = second
+second.next = third
+third.next = fourth
+fourth.next = fifth
+fifth.next = tail
+
+tmp = head
+while tmp.next:
+    print(tmp.val)
+    tmp = tmp.next
+
